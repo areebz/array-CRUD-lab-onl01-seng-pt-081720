fbacb04ba60dd179b79ee6e46186d2d3ba612dfc
Areeba Zahir <areebazahir3@gmail.com>
2020-08-29 02:18:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,7 +4,6 @@ def create_an_empty_array
  [] 
 end
 
-def create_an_array
 famous_cats = ["lil' bub", "grumpy cat", "bob", "Maru"]
 end
 
@@ -18,7 +17,6 @@ end
 def add_element_to_start_of_array(array, element)
  
 array.unshift (element)
- binding.pry
 end
 
 def remove_element_from_end_of_array(array)
