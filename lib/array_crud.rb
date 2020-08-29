db61ba120d651f2b1daa4d1e8839b74c7f46fa2b
Areeba Zahir <areebazahir3@gmail.com>
2020-08-29 01:48:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,31 +1,47 @@
 def create_an_empty_array
-  
+ [] 
 end
 
 def create_an_array
-  
+famous_cats = ["lil' bub", "grumpy cat", "bob", "Maru"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+famous_cats = ["lil' bub", "grumpy cat", "bob", "Maru"]
+ 
+famous_cats.push("arrays!")
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+famous_cats = ["lil' bub", "grumpy cat", "bob", "Maru"]
+ 
+famous_cats.unshift("am")
+ 
+puts famous_cats.inspect
 end
 
 def remove_element_from_end_of_array(array)
-  
+famous_cats = ["lil' bub", "grumpy cat", "bob", "Maru"]
+arrays = famous_cats.pop
+ 
+puts famous_cats.inspect 
+
+puts arrays
 end
 
 def remove_element_from_start_of_array(array)
-  
+  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+lil_bub = famous_cats.shift
+ 
+puts famous_cats.inspect
+# > ["grumpy cat", "Maru"]
+puts lil_bub
 end
 
-def retrieve_element_from_index(array, index_number)
-  
-end
+def retrieve_element_from_index(array,index)
 
+end
 def retrieve_first_element_from_array(array)
   
 end
