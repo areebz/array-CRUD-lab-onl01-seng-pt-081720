1e99e238e9362b75b2fe8ee1dffa719d117e1705
Areeba Zahir <areebazahir3@gmail.com>
2020-08-29 02:21:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 def remove_element_from_end_of_array(array)
 famous_cats = ["lil' bub", "grumpy cat", "bob", "Maru"]
 arrays = famous_cats.pop
- 
+ binding.pry 
 puts famous_cats.inspect 
 
 puts arrays
